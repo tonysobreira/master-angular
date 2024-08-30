aaa03e23bcf6e10dfa743f891106795ca847acc4
Tony Sobreira <tonysobreira@gmail.com>
2024-08-29 23:26:35 -0300

    section 2 complete

--- projects/01-starting-project/src/app/tasks/task/task.component.ts
+++ projects/01-starting-project/src/app/tasks/task/task.component.ts
@@ -1,20 +1,22 @@
-import { Component, Input } from '@angular/core';
+import { DatePipe } from '@angular/common';
+import { Component, inject, Input } from '@angular/core';
 
-interface Task {
-  id: string;
-  userId: string;
-  title: string;
-  summary: string;
-  dueDate: string;
-}
+import { CardComponent } from '../../shared/card/card.component';
+import { TasksService } from '../tasks.service';
+import { type Task } from './task.model';
 
 @Component({
   selector: 'app-task',
   standalone: true,
-  imports: [],
+  imports: [CardComponent, DatePipe],
   templateUrl: './task.component.html',
   styleUrl: './task.component.css',
 })
 export class TaskComponent {
   @Input({ required: true }) task!: Task;
+  private tasksService = inject(TasksService);
+
+  onCompleteTask() {
+    this.tasksService.removeTask(this.task.id);
+  }
 }
--- projects/01-starting-project/src/app/user/user.component.ts
+++ projects/01-starting-project/src/app/user/user.component.ts
@@ -1,26 +1,17 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-
-// type User = {
-//   id: string;
-//   avatar: string;
-//   name: string;
-// };
-
-interface User {
-  id: string;
-  avatar: string;
-  name: string;
-}
+import { type User } from './user.model';
+import { CardComponent } from '../shared/card/card.component';
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [],
+  imports: [CardComponent],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css',
 })
 export class UserComponent {
   @Input({ required: true }) user!: User;
+  @Input({ required: true }) selected!: boolean;
   @Output() select = new EventEmitter<string>();
 
   get imagePath() {
