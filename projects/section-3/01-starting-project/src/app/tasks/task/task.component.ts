765a77bea76b880cba76c41d4d7e0e54ee2d3671
Tony Sobreira <tonysobreira@gmail.com>
2024-09-05 17:52:08 -0300

    section 3

---
+++ projects/section-3/01-starting-project/src/app/tasks/task/task.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input, inject } from '@angular/core';
+
+import { TasksService } from '../tasks.service';
+import { type Task } from './task.model';
+
+@Component({
+  selector: 'app-task',
+  templateUrl: './task.component.html',
+  styleUrl: './task.component.css',
+})
+export class TaskComponent {
+  @Input({ required: true }) task!: Task;
+  private tasksService = inject(TasksService);
+
+  onCompleteTask() {
+    this.tasksService.removeTask(this.task.id);
+  }
+}
